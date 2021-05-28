d0fe11e500423df9e389606aa84823c2870862bf
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2021-05-28 14:28:25 -0300

    add promise

--- index.test.js
+++ index.test.js
@@ -2,8 +2,8 @@ const frisby = require('frisby');
 
 const message = 'Hello World!';
 
-test('verifica se retorna o json e status corretos ao acesssar /welcome', () => {
-  frisby.get('http://localhost:3333/welcome')
+test('verifica se retorna o json e status corretos ao acesssar /welcome', async () => {
+  await frisby.get('http://localhost:3333/welcome')
     .expect('status', 200)
     .then((response) => {
       const { body } = response;
